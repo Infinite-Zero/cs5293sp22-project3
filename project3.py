e398719ffd5f8d94e2215bff09122d33cf4bf5d9
InfiniteZero <kyle.teel@ou.edu>
2022-05-09 09:53:57 -0500

    this did not get carried to github for some reason, trying again

--- project3.py
+++ project3.py
@@ -8,7 +8,7 @@ from sklearn.feature_extraction import DictVectorizer
 
 
 
-def word_count(str):
+def wordCount(str):
     """Splits sentences into dictionaries with words labeled by occurence"""
     counts = dict()
     words = str.split()
@@ -31,7 +31,7 @@ def readData():
     # url = "https://github.com/cegme/cs5293sp22/blob/main/unredactor.tsv"
     ''''''
     items = []
-    f = open('C:\\Users\\Kyle\\PycharmProjects\\textAnalytics\\cs5293sp22\\unredactor.tsv',
+    f = open('unredactor.tsv',
              encoding='utf8').read().lower().split('\n')
     for i in f:
         i = i.split('\t')
@@ -51,13 +51,13 @@ def readData():
         # {user github name}  {set where the data belongs}  {censored name}  {sentence with censored bars over name}
         if (len(items[i]) == 4):
             if (items[i][1].lower() == "training"):
-                trainSent.append(word_count(items[i][3]))
+                trainSent.append(wordCount(items[i][3]))
                 trainCens.append(items[i][2])
             elif (items[i][1].lower() == "validation"):
-                validSent.append(word_count(items[i][3]))
+                validSent.append(wordCount(items[i][3]))
                 validCens.append(items[i][2])
             elif (items[i][1].lower() == "testing" or items[i][1].lower() == "test"):
-                testSent.append(word_count(items[i][3]))
+                testSent.append(wordCount(items[i][3]))
                 testCens.append(items[i][2])
             else:
                 namesNot.append(items[i][1])
@@ -70,46 +70,42 @@ def readData():
     return trainSent, trainCens, validSent, validCens, testSent, testCens
 
 
+def model(XtrainData, YtrainData, XtestData, YtestData):
+    # dictionary vectorizor
+    dictVec = DictVectorizer(sparse=False)
+    Xtrain = dictVec.fit_transform(XtrainData)
+    Xtest = dictVec.transform(XtestData)
+
+    # naive bayes
+    clf = MultinomialNB()
+    train = clf.fit(Xtrain, YtrainData)
+    results = train.predict(Xtest)
+
+    # print calculations
+    printResults(YtestData, results, "training")
 
 
+
+def printResults(actual, predicted, type):
+    """print the precision, recall, accuracy, and f1 score"""
+    print('Precision (%s): %.3f' % (type, precision_score(actual, predicted, average='micro')))
+    print('Recall    (%s): %.3f' % (type, recall_score(actual, predicted, average='micro')))
+    print('Accuracy  (%s): %.3f' % (type, accuracy_score(actual, predicted)))
+    print('F1 Score  (%s): %.3f' % (type, f1_score(actual, predicted, average='micro')))
+    print()
 
 
 def main():
     """Main function to start the program."""
     # reads data and puts it in arrays
     trainSent, trainCens, validSent, validCens, testSent, testCens = readData()
-    #print(trainSent[0])
 
     #training
-    dictVec = DictVectorizer(sparse=False)
-    Xtrain = dictVec.fit_transform(trainSent)
-    Xtest = dictVec.transform(testSent)
-    clf = MultinomialNB()
-
-    trainSupport = clf.fit(Xtrain, trainCens)
-    results = trainSupport.predict(Xtest)
-    printResults(testCens, results, "training")
-
+    model(trainSent, trainCens, testSent, testCens)
 
     #validation
-    dictVec2 = DictVectorizer(sparse=False)
-    Xvalid = dictVec2.fit_transform(validSent)
-    Xtest = dictVec2.transform(testSent)
-    clf2 = MultinomialNB()
-    validSupport = clf2.fit(Xvalid, validCens)
+    model(validSent, validCens, testSent, testCens)
 
-    results = validSupport.predict(Xtest)
-    printResults(testCens, results, "validation")
-
-
-
-def printResults(actual, predicted, type):
-    """print the precision, recall, accuracy, and f1 score"""
-    print('Precision (%s): %.5f' % (type, precision_score(actual, predicted, average='micro')))
-    print('Recall (%s): %.5f' % (type, recall_score(actual, predicted, average='micro')))
-    print('Accuracy (%s): %.5f' % (type, accuracy_score(actual, predicted)))
-    print('F1 Score (%s): %.5f' % (type, f1_score(actual, predicted, average='micro')))
-    print()
 
 
 if __name__ == "__main__":
